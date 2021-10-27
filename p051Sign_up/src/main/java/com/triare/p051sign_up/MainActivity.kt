b7bb940284bfe8035ca222ebb6665082859bbff9
Dima Danilov <ddvdani14@gmail.com>
2021-10-27 21:36:04 +0300

    Sign_up and Contacts xml done

---
+++ p051Sign_up/src/main/java/com/triare/p051sign_up/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.triare.p051sign_up
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
